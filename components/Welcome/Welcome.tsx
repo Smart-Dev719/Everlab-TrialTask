e13a413ddba6e0c22f1199d7dc488faf2d23c82e
Reymar <reymarpamintuan112@gmail.com>
2023-12-03 09:04:08 +0900

    Update css style

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,10 +1,9 @@
-import { Title, Text, Anchor } from '@mantine/core';
-import classes from './Welcome.module.css';
+import { Title, Text } from '@mantine/core';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title ta="center" pt={100} order={1} lts={1}>
         Trial Task for{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Everlab
